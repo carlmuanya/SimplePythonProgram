5768e7937356fbec389c16e40e6ca09d969a1ae6
Carl Muanya <91351162+carlmuanya@users.noreply.github.com>
2021-10-07 17:10:14 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,21 @@
+firstName = ' Carl'
+lastName = ' Muanya'
+
+
+print('Hello My name is' + firstName);
+
+print("What is Your Name");
+
+YourName = input( );
+
+print('Welcome ' + YourName);
+
+print('How old are you ' + YourName + '?')
+
+#This program asks for the users age to use as a variable
+myAge = input();
+
+print('You will be ' + str(int(myAge) + 1) + ' in 1 year!');
+
+
+print ('The length of Your name is: ' + str(len(YourName)) + ' letters long!');
